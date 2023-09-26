74a6d3b68a3bfff71c473051f27626da7b6765eb
Sofialuzbarbosa <112577706+Sofialuzbarbosa@users.noreply.github.com>
2023-09-27 07:58:34 +1300

    Update AlumniSelenium.java
    Cambie la forma en la que espera. Agregue:         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)); se pone al principio y no cada vez que tiene que esperar.

--- AlumniSelenium.java
+++ AlumniSelenium.java
@@ -10,12 +10,10 @@ public class Alumni {
 		
 		System.setProperty("webdriver.chrome.driver", "/Users/sofiabarbosa/Documents/chromedriver");
 		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://alumni.education/");
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.findElement(By.id("login-mail")).sendKeys("barbosasofialuz@gmail.com");
 		driver.findElement(By.id("login-password")).sendKeys("Sofisofi");
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
 		driver.findElement(By.cssSelector(".it-button.font-mulish.tw-mx-auto.dark.large")).click();		
 		System.out.println(driver.findElement(By.className("text-it-error")).getText());
 
